311239f69df2f630a4e45ae9a4c8a2333a2e02ad
ApeWithGlasses <wcgiraldo@gmail.com>
2023-03-09 21:47:48 -0500

    V 1.3

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const main = document.querySelector('main');
 
 //DBS
 const userDB = [{ username: 'admin', password: 'admin' }];
-const clientDB = [];
-const cellphoneDB = [];
-const laptopDB = [];
+const clientDB = [{ name: 'Alejandro Gutierrez', idNumber: '00000000', phoneNumber: '555-555', address: 'Calle Siempre Viva 123', email: 'alejandro@example.com' }];
+const cellphoneDB = [{ name: 'Samsung Galaxy A53', reference: '180GB', code: '12455', price: '2678431', stockQuantity: '10', productImage: 'https://m.media-amazon.com/images/I/71HN4P-pd5L.jpg' }, { name: 'Samsung Galaxy A33', reference: '240GB', code: '12456', price: '1069900', stockQuantity: '10', productImage: 'https://c8j8r9g2.rocketcdn.me/wp-content/uploads/2022/10/samsung_galaxy_a33_negro_001_l.jpeg' }];
+const laptopDB = [{name: 'ASUS Vivobook 14', reference: 'Ryzen 5 4500U - 8GB RAM - 512GB', code: '13400', price: '1781461', stockQuantity: '23', productImage: 'https://exitocol.vtexassets.com/arquivos/ids/16428889-800-auto?v=638110081125930000&width=800&height=auto&aspect=true'},{name: 'HP Intel Core i3 1115G4', reference: 'Intel Core i3 - 8GB RAM - 1TB', code: '13401', price: '1597530', stockQuantity: '45', productImage: 'https://exitocol.vtexassets.com/arquivos/ids/15994754-800-auto?v=638072550513000000&width=800&height=auto&aspect=true'}];
 
 //OBJECTS
 const createUserObject = (username, password) => {
@@ -54,6 +54,7 @@ const createLaptopObject = (name, reference, code, price, stockQuantity, product
 //SESSION ID FUNCTION
 
 let sessionId;
+
 
 //REGISTER OBJECT ALERT
 const registerAlert = () => {
@@ -161,6 +162,42 @@ const mainPage = () => {
 
     main.appendChild(mainContainer);
 
+    // IPT = INPUT
+
+    //CARD CREATE
+    const card = (productImageIpt, productNameIpt, productPriceIpt, productStockIpt, productReferenceIpt, cardContainer) => {
+        const card = document.createElement('div');
+        const imageContainer = document.createElement('div');
+        const textContainer = document.createElement('div');
+        const productName = document.createElement('p');
+        const productPrice = document.createElement('p');
+        const productStock = document.createElement('p');
+        const productReference = document.createElement('p');
+
+        //card properties
+        card.classList = 'card';
+
+        //image container properties
+        imageContainer.classList = 'image-container';
+        imageContainer.style.backgroundImage = 'url("' + productImageIpt + '")';
+
+        //text container properties
+        textContainer.classList = 'text-container';
+        productName.textContent = '' + productNameIpt + '';
+        productPrice.textContent = '$' + productPriceIpt + '';
+        productStock.textContent = 'Cantidad: ' + productStockIpt + '';
+        productReference.textContent = 'REF ' + productReferenceIpt + '';
+
+        card.appendChild(imageContainer);
+        card.appendChild(textContainer);
+        textContainer.appendChild(productName);
+        textContainer.appendChild(productPrice);
+        textContainer.appendChild(productStock);
+        textContainer.appendChild(productReference);
+
+        cardContainer.appendChild(card);
+    }
+
     //CLIENT REGISTER FORM
     const clientRegisterForm = () => {
         const formContainer = document.createElement('div');
@@ -177,6 +214,33 @@ const mainPage = () => {
         const emailLabel = document.createElement('label');
         const emailInput = document.createElement('input');
         const submitBtn = document.createElement('input');
+        const tableContainer = document.createElement('div');
+        const rowContainer = document.createElement('table');
+
+        // table
+        const table = document.createElement('table');
+        const headerRow = document.createElement('tr');
+        const nameHeader = document.createElement('th');
+        const idNumberHeader = document.createElement('th');
+        const phoneNumberHeader = document.createElement('th');
+        const addressHeader = document.createElement('th');
+        const emailHeader = document.createElement('th');
+
+        //properties
+        nameHeader.textContent = 'Nombre';
+        idNumberHeader.textContent = 'Número de identificación';
+        phoneNumberHeader.textContent = 'Número de contacto';
+        addressHeader.textContent = 'Dirección';
+        emailHeader.textContent = 'Correo electrónico';
+
+        headerRow.appendChild(nameHeader);
+        headerRow.appendChild(idNumberHeader);
+        headerRow.appendChild(phoneNumberHeader);
+        headerRow.appendChild(addressHeader);
+        headerRow.appendChild(emailHeader);
+
+        table.appendChild(headerRow);
+        tableContainer.appendChild(table);
 
         formContainer.classList = 'form-container';
         formContainer.appendChild(form);
@@ -189,6 +253,7 @@ const mainPage = () => {
         nameLabel.innerHTML = 'Nombre completo';
         nameInput.type = 'text';
         nameInput.required = true;
+        nameInput.autofocus = true;
 
         nameLabel.appendChild(nameInput);
 
@@ -224,6 +289,9 @@ const mainPage = () => {
         submitBtn.value = 'Registrar cliente';
         submitBtn.type = 'submit';
 
+        //table container properties
+        tableContainer.classList = 'table-container';
+
         form.appendChild(nameLabel);
         form.appendChild(idNumberLabel);
         form.appendChild(phoneNumberLabel);
@@ -232,34 +300,294 @@ const mainPage = () => {
         form.appendChild(submitBtn);
 
         container.appendChild(formContainer);
+        container.appendChild(tableContainer);
+
+        const clientTable = () => {
+            clientDB.forEach(client => {
+                const row = document.createElement('tr');
+
+                row.classList = 'row';
+
+                const nameCell = document.createElement('td');
+                const idNumberCell = document.createElement('td');
+                const phoneNumberCell = document.createElement('td');
+                const addressCell = document.createElement('td');
+                const emailCell = document.createElement('td');
+
+                // properties
+                nameCell.textContent = client.name;
+                idNumberCell.textContent = client.idNumber;
+                phoneNumberCell.textContent = client.phoneNumber;
+                addressCell.textContent = client.address;
+                emailCell.textContent = client.email;
+
+                row.appendChild(nameCell);
+                row.appendChild(idNumberCell);
+                row.appendChild(phoneNumberCell);
+                row.appendChild(addressCell);
+                row.appendChild(emailCell);
+
+                rowContainer.appendChild(row);
+            });
+
+            tableContainer.appendChild(rowContainer);
+        }
+
+        clientTable();
 
         submitBtn.addEventListener('click', () => {
             event.preventDefault();
-            
-            const client = createClientObject(nameInput.value, idNumberInput.value, phoneNumberInput.value, addressInput.value, emailInput.value);
-
-            registerAlert();
-
-            clientDB.push(client);
-
+            const removeRows = () => {
+                rowContainer.innerHTML = '';
+            }
+            removeRows();
+            if (nameInput.value !== '' && idNumberInput.value !== '' && phoneNumberInput.value !== '' && addressInput.value !== '' && emailInput.value !== '') {
+                const client = createClientObject(nameInput.value, idNumberInput.value, phoneNumberInput.value, addressInput.value, emailInput.value);
+                registerAlert();
+                clientDB.push(client);
+            } else {
+                alert('No puedes ingresar un elemento vacío.');
+            }
             nameInput.value = '';
             idNumberInput.value = '';
             phoneNumberInput.value = '';
             addressInput.value = '';
             emailInput.value = '';
 
-            console.log(clientDB);
+            clientTable();
         });
     }
 
     //PHONE REGISTER FORM
     const phoneRegisterForm = () => {
+        const formContainer = document.createElement('div');
+        const form = document.createElement('form');
+        const title = document.createElement('h1');
+        const nameLabel = document.createElement('label');
+        const nameInput = document.createElement('input');
+        const referenceLabel = document.createElement('label');
+        const referenceInput = document.createElement('input');
+        const codeLabel = document.createElement('label');
+        const codeInput = document.createElement('input');
+        const priceLabel = document.createElement('label');
+        const priceInput = document.createElement('input');
+        const stockQuantityLabel = document.createElement('label');
+        const stockQuantityInput = document.createElement('input');
+        const productImageLabel = document.createElement('label');
+        const productImageInput = document.createElement('input');
+        const submitBtn = document.createElement('input');
+        const cardTitle = document.createElement('h1');
+        const cardContainer = document.createElement('div');
 
+        formContainer.classList = 'form-container';
+        formContainer.appendChild(form);
+
+        title.innerHTML = 'REGISTRAR CELULAR';
+
+        container.appendChild(title);
+
+        //name label/input properties
+        nameLabel.innerHTML = 'Nombre producto:';
+        nameInput.type = 'text';
+        nameInput.required = true;
+        nameInput.autofocus = true;
+
+        //reference label/input properties
+        referenceLabel.innerHTML = 'Número de referencia:';
+        referenceInput.type = 'text';
+        referenceInput.required = true;
+
+        //code label/input properties
+        codeLabel.innerHTML = 'Código:';
+        codeInput.type = 'text';
+        codeInput.required = true;
+
+        //price label/input properties
+        priceLabel.innerHTML = 'Precio (sin comas):';
+        priceInput.type = 'text';
+        priceInput.required = true;
+
+        //stockQuantity label/input properties
+        stockQuantityLabel.innerHTML = 'Cantidad en stock (sin comas ni puntos):';
+        stockQuantityInput.type = 'text';
+        stockQuantityInput.required = true;
+
+        //productimage label/input properties
+        productImageLabel.innerHTML = 'Ingrese la imagen del producto:';
+        productImageInput.type = 'text';
+        productImageInput.required = true;
+
+        //submit btn properties
+        submitBtn.type = 'submit';
+        submitBtn.value = 'Registrar celular';
+
+        //card container properties
+        cardContainer.classList = 'card-container';
+        cardTitle.textContent = 'Celulares registrados';
+
+        nameLabel.appendChild(nameInput);
+        referenceLabel.appendChild(referenceInput);
+        codeLabel.appendChild(codeInput);
+        priceLabel.appendChild(priceInput);
+        stockQuantityLabel.appendChild(stockQuantityInput);
+        productImageLabel.appendChild(productImageInput);
+
+        form.appendChild(nameLabel);
+        form.appendChild(referenceLabel);
+        form.appendChild(codeLabel);
+        form.appendChild(priceLabel);
+        form.appendChild(stockQuantityLabel);
+        form.appendChild(productImageLabel);
+        form.appendChild(submitBtn);
+
+        container.appendChild(formContainer);
+        container.appendChild(cardTitle);
+        container.appendChild(cardContainer);
+
+        const phoneCardShow = () => {
+            cellphoneDB.forEach(cellphone => {
+                card(cellphone.productImage, cellphone.name, cellphone.price, cellphone.stockQuantity, cellphone.reference, cardContainer);
+            });
+        }
+
+        phoneCardShow();
+
+        submitBtn.addEventListener('click', () => {
+            event.preventDefault();
+            if (nameInput.value !== '' && referenceInput.value !== '' && codeInput.value !== '' && priceInput.value !== '' && stockQuantityInput.value !== '' && productImageInput.value !== '') {
+                const cellphone = createCellphoneObject(nameInput.value, referenceInput.value, codeInput.value, priceInput.value, stockQuantityInput.value, productImageInput.value);
+                registerAlert();
+                cellphoneDB.push(cellphone);
+            } else {
+                alert('No puedes ingresar un elemento vacío.');
+            }
+            nameInput.value = '';
+            referenceInput.value = '';
+            codeInput.value = '';
+            priceInput.value = '';
+            stockQuantityInput.value = '';
+            productImageInput.value = '';
+
+            cardContainer.innerHTML = '';
+
+            phoneCardShow();
+        });
     }
 
     //LAPTOP REGISTER FORM
     const laptopRegisterForm = () => {
+        const formContainer = document.createElement('div');
+        const form = document.createElement('form');
+        const title = document.createElement('h1');
+        const nameLabel = document.createElement('label');
+        const nameInput = document.createElement('input');
+        const referenceLabel = document.createElement('label');
+        const referenceInput = document.createElement('input');
+        const codeLabel = document.createElement('label');
+        const codeInput = document.createElement('input');
+        const priceLabel = document.createElement('label');
+        const priceInput = document.createElement('input');
+        const stockQuantityLabel = document.createElement('label');
+        const stockQuantityInput = document.createElement('input');
+        const productImageLabel = document.createElement('label');
+        const productImageInput = document.createElement('input');
+        const submitBtn = document.createElement('input');
+        const cardTitle = document.createElement('h1');
+        const cardContainer = document.createElement('div');
 
+        formContainer.classList = 'form-container';
+        formContainer.appendChild(form);
+
+        title.innerHTML = 'Registrar portátil';
+
+        container.appendChild(title);
+
+        //name label/input properties
+        nameLabel.innerHTML = 'Nombre producto:';
+        nameInput.type = 'text';
+        nameInput.required = true;
+        nameInput.autofocus = true;
+
+        //reference label/input properties
+        referenceLabel.innerHTML = 'Número de referencia:';
+        referenceInput.type = 'text';
+        referenceInput.required = true;
+
+        //code label/input properties
+        codeLabel.innerHTML = 'Código:';
+        codeInput.type = 'text';
+        codeInput.required = true;
+
+        //price label/input properties
+        priceLabel.innerHTML = 'Precio (sin comas):';
+        priceInput.type = 'text';
+        priceInput.required = true;
+
+        //stockQuantity label/input properties
+        stockQuantityLabel.innerHTML = 'Cantidad en stock (sin comas ni puntos):';
+        stockQuantityInput.type = 'text';
+        stockQuantityInput.required = true;
+
+        //productimage label/input properties
+        productImageLabel.innerHTML = 'Ingrese la imagen del producto:';
+        productImageInput.type = 'text';
+        productImageInput.required = true;
+
+        //submit btn properties
+        submitBtn.type = 'submit';
+        submitBtn.value = 'Registrar portátil';
+
+        //card container properties
+        cardContainer.classList = 'card-container';
+        cardTitle.textContent = 'Portátiles registrados';
+
+        nameLabel.appendChild(nameInput);
+        referenceLabel.appendChild(referenceInput);
+        codeLabel.appendChild(codeInput);
+        priceLabel.appendChild(priceInput);
+        stockQuantityLabel.appendChild(stockQuantityInput);
+        productImageLabel.appendChild(productImageInput);
+
+        form.appendChild(nameLabel);
+        form.appendChild(referenceLabel);
+        form.appendChild(codeLabel);
+        form.appendChild(priceLabel);
+        form.appendChild(stockQuantityLabel);
+        form.appendChild(productImageLabel);
+        form.appendChild(submitBtn);
+
+        container.appendChild(formContainer);
+        container.appendChild(cardTitle);
+        container.appendChild(cardContainer);
+
+        const laptopCardShow = () => {
+            laptopDB.forEach(laptop => {
+                card(laptop.productImage, laptop.name, laptop.price, laptop.stockQuantity, laptop.reference, cardContainer);
+            });
+        }
+
+        laptopCardShow();
+
+        submitBtn.addEventListener('click', () => {
+            event.preventDefault();
+            if (nameInput.value !== '' && referenceInput.value !== '' && codeInput.value !== '' && priceInput.value !== '' && stockQuantityInput.value !== '' && productImageInput.value !== '') {
+                const laptop = createCellphoneObject(nameInput.value, referenceInput.value, codeInput.value, priceInput.value, stockQuantityInput.value, productImageInput.value);
+                registerAlert();
+                laptopDB.push(laptop);
+            } else {
+                alert('No puedes ingresar un elemento vacío.');
+            }
+            nameInput.value = '';
+            referenceInput.value = '';
+            codeInput.value = '';
+            priceInput.value = '';
+            stockQuantityInput.value = '';
+            productImageInput.value = '';
+
+            cardContainer.innerHTML = '';
+
+            laptopCardShow();
+        });
     }
 
     //button behaviors
@@ -317,6 +645,7 @@ const loginForm = () => {
     usernameLabel.innerHTML = 'Usuario:';
     usernameInput.type = 'text';
     usernameInput.required = true;
+    usernameInput.autofocus = true;
 
     //password label/input properties
     passwordLabel.htmlFor = 'password';
@@ -418,6 +747,7 @@ const registerForm = () => {
     usernameLabel.innerHTML = 'Ingrese un usuario:';
     usernameInput.type = 'text';
     usernameInput.required = true;
+    usernameInput.autofocus = true;
 
     //password label/input properties
     passwordLabel.htmlFor = 'password';
